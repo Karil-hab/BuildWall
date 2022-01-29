de84cf33cbf7e268a18d62988fb853c0f6b5ddbe
Karil-hab <58692947+Karil-hab@users.noreply.github.com>
2022-01-29 16:46:52 +0900

    Add files via upload

---
+++ HeadContr.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeadContr : MonoBehaviour
+{
+    Transform head_tr;
+
+    float MouseX;
+    float MouseY;
+
+    public GameObject FirstrCamera;
+    public GameObject ThirdCamera;
+    public float sens = 3f;
+
+    bool setActive = true; 
+    
+    void Start()
+    {
+        head_tr = GetComponent<Transform>();
+    }
+
+
+    void Update()
+    {
+        MouseX = MouseX + Input.GetAxis("Mouse X") * sens;
+        MouseY = MouseY - Input.GetAxis("Mouse Y") * sens;
+
+        MouseY = Mathf.Clamp(MouseY, -30, 30);
+
+        head_tr.rotation = Quaternion.Euler(MouseY, MouseX, 0);
+
+        FindObjectOfType<BodyContr>().Poluchatel(MouseX);
+
+        if(Input.GetKeyDown("c"))
+        {
+            setActive = !setActive;
+
+            ThirdCamera.SetActive(setActive);
+            FirstrCamera.SetActive(!setActive);  
+
+            /*if(setActive==true)
+            {
+                ThirdCamera.SetActive(true);
+                FirstrCamera.SetActive(false);  
+                setActive = false;
+            }
+            else
+            {
+                ThirdCamera.SetActive(false);
+                FirstrCamera.SetActive(true);  
+                setActive = true;
+            }*/
+        }
+    }
+}
---
+++ SpawnContr.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnContr : MonoBehaviour
+{
+    public GameObject bullet;
+    Transform TRbulet;
+    GameObject clonBullet;
+    Rigidbody RBbulet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TRbulet = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        clonBullet = Instantiate(bullet,TRbulet.position,TRbulet.rotation);
+        RBbulet = clonBullet.GetComponent<Rigidbody>();
+        RBbulet.AddForce(TRbulet.forward * 1000f);
+        Destroy(clonBullet,3);
+    }
+}
